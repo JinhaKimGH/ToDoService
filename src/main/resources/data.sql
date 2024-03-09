706a93771fc49981f3d89ba578c6f5f3a53e6d42
Jinha <56976518+JinhaKimGH@users.noreply.github.com>
2024-03-09 01:12:45 -0500

    Initial project commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID,USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'jinha', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID,USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'jinha', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID,USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'jinha', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID,USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'jinha', 'Learn DevOps', CURRENT_DATE(), false);
